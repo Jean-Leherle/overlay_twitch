8c9f0232f754dbac22af91f0822fb9b35ca9bbf2
jean-leherle <j.leherle@gmail.com>
2024-12-22 23:39:08 +0100

    creation de gear (incomplet)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
-import './style.css'
+import './style.css';
+import { Gear } from './component/gear';
+import gearSVG from '/gear-svgrepo-com.svg'
+// Exemple de SVG pour un engrenage
 
+// const gearSVG = `
+// <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="100" height="100">
+//   <circle cx="50" cy="50" r="40" fill="none" stroke="black" stroke-width="4" />
+//   <path d="M50,10 L55,30 L45,30 Z" fill="black" />
+// </svg>`;
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-  hello world
-  </div>
-`
+const app = document.querySelector<HTMLDivElement>('#app')!;
 
+// Instancier un engrenage
+const gear1 = new Gear(app, {
+  teeth: 12,
+  radius: 50,
+  rotationSpeed: 60,
+  clockwise: true,
+  position: { x: 100, y: 100 },
+}, gearSVG);
+
+// Exemple : déplacer et ralentir l'engrenage après 3 secondes
+setTimeout(() => {
+  gear1.updateProps({
+    rotationSpeed: 30,
+    clockwise: false,
+    position: { x: 200, y: 150 },
+  });
+}, 3000);
